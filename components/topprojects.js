a4003c3e6e48e9a6b0e845efe06f293ca7db1e37
Mert Doğu <mertdoguonder@protonmail.com>
2022-07-11 16:28:20 +0300

    add: components

--- components/topprojects.js
+++ components/topprojects.js
@@ -1,8 +1,10 @@
-export default function Main() {
+export default function topprojects() {
     return (
         <div className="flex -space-x-3">
             {CONFIG.projects.map((r) => (
-                <img src={r.img} className={r.zindex + ' w-10 rounded-xl border-4 border-white hover:-translate-y-2 transition-all'} alt={r.name + 'image'}/>
+                <a className={r.zindex} href={r.link}>
+                    <img src={r.img} className='w-10 rounded-xl border-4 border-white hover:-translate-y-2 transition-all' alt={r.name + 'image'}/>
+                </a>
             ))}
         </div>
     )
@@ -13,19 +15,19 @@ export const CONFIG = {
         {
             Name:"SettleCo",
             img: "/meta/settleco.png",
-            link:"https://github.com/rexulec",
+            link:"https://settleco.net",
             zindex:"z-30"
         },
         {
             Name:"Isolation",
             img: "/meta/isolation.png",
-            link:"https://github.com/rexulec",
+            link:"https://isolation.settleco.net",
             zindex:"z-20"
         },
         {
             Name:"MergeApp",
             img: "/meta/mergeapp.png",
-            link:"https://github.com/rexulec",
+            link:"https://mergeapp.dev",
             zindex:"z-10"
         },
     ]
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Topprojects from '../components/topprojects'
+import Topmain from '../components/topmain'
+
 
 export default function Home() {
   return (
+
     <div className="grid justify-items-center pt-52">
         <div className="w-5/12">
-            <Topprojects />
+            <div className="space-y-4">
+                <Topprojects />
+                <Topmain />
+            </div>
+            <div>
+
+            </div>
         </div>
     </div>
   )
