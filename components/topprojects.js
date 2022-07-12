60cbc1b87ac6ff77b102c9372c38c8b089188a0a
Mert Doğu <mertdoguonder@protonmail.com>
2022-07-12 17:27:13 +0300

    add: OpenAnime into top bar

--- components/topprojects.js
+++ components/topprojects.js
@@ -3,7 +3,7 @@ export default function topprojects() {
         <div className="flex -space-x-3">
             {CONFIG.projects.map((r) => (
                 <a className={r.zindex} href={r.link}>
-                    <img src={r.img} className='w-10 rounded-xl border-4 border-white hover:-translate-y-2 transition-all' alt={r.name + 'image'}/>
+                    <img src={r.img} className='w-10 h-10 rounded-xl border-4 border-white hover:-translate-y-2 transition-all' alt={r.name + 'image'}/>
                 </a>
             ))}
         </div>
@@ -16,6 +16,12 @@ export const CONFIG = {
             Name:"SettleCo",
             img: "/meta/settleco.png",
             link:"https://settleco.net",
+            zindex:"z-40"
+        },
+        {
+            Name:"OpenAnime",
+            img: "/meta/openanime.png",
+            link:"https://openani.me",
             zindex:"z-30"
         },
         {
