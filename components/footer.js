18b031871456758b1a7a3195196bdebc5f3d2c90
Mert Doğu <mertdoguonder@protonmail.com>
2022-07-12 18:05:00 +0300

    add: footer

---
+++ components/footer.js
@@ -0,0 +1,8 @@
+export default function topmain() {
+    return (
+        <div className="flex justify-between text-stone-800 py-10">
+            <p>Made with Next</p>
+            <p>rexulec.com</p>
+        </div>
+    )
+}
