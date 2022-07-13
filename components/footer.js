a4b8c17258a0cb8aa9ce0373ed1721c6c6b757e4
Mert Doğu <mertdoguonder@protonmail.com>
2022-07-13 11:57:48 +0300

    feat: added hover effects

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,13 @@
 export default function topmain() {
     return (
         <div className="flex justify-between text-stone-800 py-10">
-            <p>Made with Next</p>
-            <p>rexulec.com</p>
+            <div>
+                <p>Developed & Hosted by Mert Doğu</p>
+            </div>
+            <div className="space-x-2 text-stone-600">
+                <a className="hover:text-stone-800 transition" href="https://read.cv/rexulec">Read.cv</a>
+                <a className="hover:text-stone-800 transition" href="https://linkedin.com/rexulec">LinkedIn</a>
+            </div>
         </div>
     )
 }
